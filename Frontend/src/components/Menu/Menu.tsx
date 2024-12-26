f6c7fe750e0db745e099f5ec0cc375c76921888b
Raunak Shah <shahrrs2004@gmail.com>
2024-12-26 23:15:59 +0530

    Fixed Profile Menu

--- Frontend/src/components/Menu/Menu.tsx
+++ Frontend/src/components/Menu/Menu.tsx
@@ -2,10 +2,13 @@ import React, { Dispatch, SetStateAction, useEffect } from "react";
 import QRCode from "../QRCode/QRCode";
 import "./Menu.css";
 import { Box } from "@mui/material";
+import { useAppContext } from "../../context/AppContext";
 
 const Menu = () =>{
+    const {screenWidth} = useAppContext()
+    const classname = screenWidth>760?"Menu": "Menularge";
     return (
-        <Box className="Menu">
+        <Box className={classname}>
             <div className="qrcodeArea"><QRCode /></div>
             <div className="profileButton">Profile</div>
         </Box>
